d9f062b68bc26ebfbbacbd41846ebdb3d687ab06
jdmedlock <jdmedlock@gmail.com>
2021-12-08 14:11:46 -0600

    Feature: Add user messages to txns

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,37 +5,66 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-  uint256 totalWaves;
-  address[] wavers;
+    uint256 totalWaves;
 
-  constructor() {
-    console.log("Here's my first smart contract using Solidity!");
-  }
+    /*
+     * A little magic, Google what events are in Solidity!
+     */
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
-  function wave() public {
-    totalWaves += 1;
-    pushWaver(msg.sender);
-    // console.log("%s has waved!", msg.sender);
-  }
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
 
-  function getTotalWaves() public view returns (uint256) {
-    // console.log("We have %d total waves!", totalWaves);
-    return totalWaves;
-  }
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
 
-  // Retrieve a specific element from the wavers array based on its position.
-  function getWaver(uint elementIndex) public view returns(address) {
-    return wavers[elementIndex];
-  }
+    constructor() {
+        console.log("I AM SMART CONTRACT. POG.");
+    }
 
-  // Retrieve the number of wavers
-  function getNoWavers() public view returns(uint) {
-    return wavers.length;
-  }
+    /*
+     * You'll notice I changed the wave function a little here as well and
+     * now it requires a string called _message. This is the message our user
+     * sends us from the frontend!
+     */
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        console.log("%s waved w/ message %s", msg.sender, _message);
 
-  // Append to the array of those who have waved
-  // This will increase the array length by 1.
-  function pushWaver(address elementValue) public {
-    wavers.push(elementValue);
-  }
+        /*
+         * This is where I actually store the wave data in the array.
+         */
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * I added some fanciness here, Google it and try to figure out what it is!
+         * Let me know what you learn in #general-chill-chat
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,46 +1,35 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners()
-  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
-  const waveContract = await waveContractFactory.deploy()
-  await waveContract.deployed()
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-  console.log("\nContract deployed to:", waveContract.address)
-  console.log("Contract deployed by:", owner.address)
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  let waverCount = 0
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  const listWavers = async (waveContract) => {
-    waverCount = await waveContract.getNoWavers()
-    console.log(`\nWe've been waved at ${ waverCount } times!`)
-    for (waverNo = 0; waverNo < waverCount; waverNo++) {
-      let waver = await waveContract.getWaver(waverNo)
-      console.log("...waved at by :", waver)
-    } 
-  }
-  
-  waveCount = await waveContract.getTotalWaves()
-  
-  let waveTxn = await waveContract.wave()
-  await waveTxn.wait()
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  waveCount = await waveContract.getTotalWaves()
-  await listWavers(waveContract)
-
-  waveTxn = await waveContract.connect(randomPerson).wave()
-  await waveTxn.wait()
-
-  waveCount = await waveContract.getTotalWaves()
-  await listWavers(waveContract)
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0)
+    process.exit(0);
   } catch (error) {
-    console.log(error)
-    process.exit(1)
+    console.log(error);
+    process.exit(1);
   }
-}
+};
 
-runMain()
+runMain();
