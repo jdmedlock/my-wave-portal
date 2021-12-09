7c43627ba7c83e8bad7400174437d6e8083ce349
Jim Medlock <jdmedlock@gmail.com>
2021-12-08 19:13:21 -0600

    Feature: Update for Section 4 - Cooldown

--- scripts/run.js
+++ scripts/run.js
@@ -1,46 +1,46 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners()
-  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
-  const waveContract = await waveContractFactory.deploy()
-  await waveContract.deployed()
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-  console.log("\nContract deployed to:", waveContract.address)
-  console.log("Contract deployed by:", owner.address)
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-  let waverCount = 0
+  /*
+   * Let's try two waves now
+   */
+  const waveTxn = await waveContract.wave('This is wave #1');
+  await waveTxn.wait();
 
-  const listWavers = async (waveContract) => {
-    waverCount = await waveContract.getNoWavers()
-    console.log(`\nWe've been waved at ${ waverCount } times!`)
-    for (waverNo = 0; waverNo < waverCount; waverNo++) {
-      let waver = await waveContract.getWaver(waverNo)
-      console.log("...waved at by :", waver)
-    } 
-  }
-  
-  waveCount = await waveContract.getTotalWaves()
-  
-  let waveTxn = await waveContract.wave()
-  await waveTxn.wait()
+  const waveTxn2 = await waveContract.wave('This is wave #2');
+  await waveTxn2.wait();
 
-  waveCount = await waveContract.getTotalWaves()
-  await listWavers(waveContract)
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-  waveTxn = await waveContract.connect(randomPerson).wave()
-  await waveTxn.wait()
-
-  waveCount = await waveContract.getTotalWaves()
-  await listWavers(waveContract)
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0)
+    process.exit(0);
   } catch (error) {
-    console.log(error)
-    process.exit(1)
+    console.log(error);
+    process.exit(1);
   }
-}
+};
 
-runMain()
+runMain();
